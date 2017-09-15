9dbc7bc0f5e64bc3f146d7f32b33ed30e9f69cf8
LizzieHerman <lizziemn96@gmail.com>
2017-09-15 10:39:23 -0600

    Initial Commit

---
+++ src/arffconverter/ARFFConverter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arffconverter;
+
+/**
+ *
+ * @author Lizzie Herman
+ */
+public class ARFFConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("HI");
+    }
+
+}
